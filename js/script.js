58f96798dadf67e762245c03baebf32872a55124
steftes99 <stefanotescione83@gmail.com>
2021-06-21 15:58:02 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const gatti = [
+    {
+        nome: 'Birba',
+        anni: 3,
+        colore: '#000000',
+        genere: 'femmina'
+    },
+    {
+        nome: 'Micio',
+        anni: 4,
+        colore: '#ffffff',
+        genere: 'maschio'
+    },
+    {
+        nome: 'Pippo',
+        anni: 8,
+        colore: '#AAA5A2',
+        genere: 'maschio'
+    },
+    {
+        nome: 'Mimì',
+        anni: 2.5,
+        colore: '#F75C1E',
+        genere: 'femmina'
+    },
+    {
+        nome: 'Penny',
+        anni: 5,
+        colore: '#AAA5A2',
+        genere: 'femmina'
+    },
+    {
+        nome: 'Ghemon',
+        anni: 6,
+        colore: '#000000',
+        genere: 'maschio'
+    }
+]
